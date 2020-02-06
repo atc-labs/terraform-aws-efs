051bdaaa826685b861781ed1a1dcae7dff7087f5
root <root@cid-artifacts-dev.to0oc1tqa1wezj1s0krsrwcobd.bx.internal.cloudapp.net>
2020-02-06 18:07:05 +0000

    added Environment string to Description

--- security_group.tf
+++ security_group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "security_group" {
   name                   = "efs-${var.project-abbr}-${var.function-abbr}-${var.organizational_unit-abbr}-${local.region-abbr}"
-  description            = "EFS - ${var.project} - ${var.function} - ${var.organizational_unit} (${local.region})"
+  description            = "EFS - ${var.project} - ${var.function} - ${var.organizational_unit} [${var.environment}] (${local.region})"
   vpc_id                 = var.vpc-id
   revoke_rules_on_delete = true
   tags = merge(
